71c7e9297a00a13801553dcf3ddaf7695f6726e3
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2023-12-02 13:22:36 +0200

    filter todos - part3 done

--- todolist.js
+++ todolist.js
@@ -7,7 +7,7 @@ const todos = [];
 const todoInput = document.querySelector(".todo-input");
 const todoForm = document.querySelector(".todo-form");
 const todoList = document.querySelector(".todolist");
-const selectFilter = document.querySelector(".selectFilter"); 
+const selectFilter = document.querySelector(".filter-todos"); 
 
 
 //events 
@@ -36,39 +36,54 @@ function addNewTodo(e) {
 
 function createTodos(todos) {
     // create todos on DOM
-let result = ""; 
-todos.forEach((todo) => {
-    result += `<li class="todo">
-    <p class="todo__title">${todo.title}</p>
-    <span class="todo__createdAt">${new Date(todo.createdAt).toLocaleDateString("fa-IR")}
-    </span>
-    <button data-todo-id=${todo.id}><i class="todo__check far fa-check-square"></i></button>
-    <button data-todo-id=${todo.id}><i class="todo__remove far fa-trash-alt"></i></button>
-  </li>`; 
-}); 
-todoList.innerHTML = result;
-todoInput.value = ""; 
-}
+    let result = "";
+    todos.forEach((todo) => {
+      result += `<li class="todo">
+        <p class="todo__title ${todo.isCompleted && "completed"}">${
+        todo.title
+      }</p>
+        <span class="todo__createdAt">${new Date(
+          todo.createdAt
+        ).toLocaleDateString("fa-IR")}</span>
+        <button class="todo__check" data-todo-id=${
+          todo.id
+        } ><i class="far fa-check-square"></i></button>
+        <button class="todo__remove" data-todo-id=${
+          todo.id
+        } ><i class="far fa-trash-alt"></i></button>
+      </li>`;
+    });
+  
+    todoList.innerHTML = result;
+    todoInput.value = "";
+  
+    const removeBtns = [...document.querySelectorAll(".todo__remove")];
+    removeBtns.forEach((btn) => btn.addEventListener("click", removeTodo));
+  
+    const checkBtns = [...document.querySelectorAll(".todo__check")];
+    checkBtns.forEach((btn) => btn.addEventListener("click", checkTodo));
+  }
 
-function filterTodos (e) {
-    console.log(e.target.value); 
-    const filter = e.target.value 
-    switch(filter){
-        case "all" :{
-            createTodos(todos); 
-            break; 
-        }
-        case "completed" :{
-            const filteredTodo = todos.filter((t) => t.isCompleted); 
-            createTodos(filterTodos); 
-            break; 
-        }
-        case "uncompleted" :{ 
-            const filteredTodo = todos.filter((t) => !t.isCompleted); 
-            createTodos(filterTodos); 
-            break;    
-        }
-        default:
-            createTodos(todos);
+function filterTodos(e) {
+    // console.log(e.target.value);
+    // const filter = e.target.value;
+    const filter = e.target.value;
+    switch (filter) {
+      case "all": {
+        createTodos(todos);
+        break;
+      }
+      case "completed": {
+        const filteredTodos = todos.filter((t) => t.isCompleted);
+        createTodos(filteredTodos);
+        break;
+      }
+      case "uncompleted": {
+        const filteredTodos = todos.filter((t) => !t.isCompleted);
+        createTodos(filteredTodos);
+        break;
+      }
+      default:
+        createTodos(todos);
     }
-}
+  }
